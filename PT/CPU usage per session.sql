0c36413628b3b16835d22caadf977737cbd6635f
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2024-07-19 21:16:17 +0530

    Create CPU usage per session.sql

---
+++ PT/CPU usage per session.sql
@@ -0,0 +1,5 @@
+select ss.username, se.SID, VALUE/100 cpu_usage_seconds
+from v$session ss, v$sesstat se, v$statname sn
+where se.STATISTIC# = sn.STATISTIC# and NAME like '%CPU used by this session%'
+and se.SID = ss.SID and ss.status='ACTIVE' ss.username is not null
+order by VALUE desc;
