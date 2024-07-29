885280f16cb5b3e4bae6e27983559182c5d6c9f0
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2024-07-29 20:28:44 +0530

    Create healthcheck.sql

---
+++ PT/healthcheck.sql
@@ -0,0 +1,13 @@
+The below-mentioned query can help you to fetch the AWR report:-
+SQL> @$ORACLE_HOME/rdbms/admin/awrrpt.sql
+
+DB uptime
+CPU utilization, memory utilization, etc.
+Listener status of the database
+FRA utilization
+Tablespaces utilization
+Database status
+Blocking sessions.
+Longrunning session.
+
+https://dbsguru.com/how-to-perform-a-health-check-of-an-oracle-database/#google_vignette
