f0ee770e699bf05a50394d020e00abb00fd179d7
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2024-09-25 22:29:49 +0530

    Create 01.sql

---
+++ RAC/RACClass/01.sql
@@ -0,0 +1,4 @@
+systemctl disable firewalld --now
+vi /etc/selinux/config (change value from enforcing to disabled)
+vi /etc/hosts (this setup only on personal desktop or laptop, in company its corporate dns setup)
+1-28
