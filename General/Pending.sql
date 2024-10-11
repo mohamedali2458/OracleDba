731918efe4f62851c49531f5a74b5a6e355af082
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2024-10-11 18:57:01 +0530

    Update Pending.sql

--- General/Pending.sql
+++ General/Pending.sql
@@ -1,1 +1,3 @@
 select ‘alter system kill session(‘||sid||’,’||serial#||’);’ from v$session where status=‘INACTIVE’;
+Select ‘alter system kill session(‘||sid||’,’||serial#||’);’ from v$session where status=‘INACTIVE’ and username=‘&username’;
+Select ‘alter system kill session‘||’’’’||sid||’,’||serial#||’,@’||inst_id||’’’’||’;’ from gv$session where status=‘INACTIVE’ and username=‘&username’;
