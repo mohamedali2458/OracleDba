ad406c5a9c404d59dcfbc34e400ae8701436b400
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2024-10-11 19:30:21 +0530

    Update Pending.sql

--- General/Pending.sql
+++ General/Pending.sql
@@ -1,3 +1,10 @@
 select ‘alter system kill session(‘||sid||’,’||serial#||’);’ from v$session where status=‘INACTIVE’;
 Select ‘alter system kill session(‘||sid||’,’||serial#||’);’ from v$session where status=‘INACTIVE’ and username=‘&username’;
 Select ‘alter system kill session‘||’’’’||sid||’,’||serial#||’,@’||inst_id||’’’’||’;’ from gv$session where status=‘INACTIVE’ and username=‘&username’;
+
+Hi team, if audit is enabled on a db, then how to find which user has done the dml change on a table
+Dba_audit_trail
+
+Any help on ORA-01552: cannot use system rollback segment for non-system tablespace
+Check UNDO tablespace utilisation
+
