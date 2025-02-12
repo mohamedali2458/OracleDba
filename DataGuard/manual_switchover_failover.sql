8514bc9ffb807cc53dfc3652094e183503c036ee
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2025-02-12 12:33:31 +0530

    Update manual_switchover_failover.sql

--- DataGuard/manual_switchover_failover.sql
+++ DataGuard/manual_switchover_failover.sql
@@ -36,11 +36,13 @@ alter database commit to switchover to physical standby with session shutdown;
 
 startup mount;
 
-At this stage, there is no primary to accept queries from client. Run below query on client putty terminal. The query will hang and wait until standby is converted to primary
+At this stage, there is no primary to accept queries from client. Run below query 
+on client putty terminal. The query will hang and wait until standby is converted to primary
 
 select name, open_mode, db_unique_name, database_role from v$database;
 
-Convert standby to primary: Our primary is already converted to standby. Now it’s time to convert original standby into primary
+Convert standby to primary: Our primary is already converted to standby. Now it’s time 
+to convert original standby into primary
 
 select SWITCHOVER_STATUS from V$DATABASE;
 
@@ -55,16 +57,17 @@ On new standby – Initially your primary database: Start MRP
 
 alter database recover managed standby database disconnect;
 
-Revert back: Once again follow the above process from top and re-execute steps in proper databases to revert back to original setup.
+Revert back: Once again follow the above process from top and re-execute steps 
+in proper databases to revert back to original setup.
 
 
-Failover
-========
-Performing Manual Failover on Physical Standby
-
-Failover is when your primary database is completely lost. When there is a failover, standby is converted into primary but primary 
-is not converted into standby as it is lost. If you do not have Flashback enabled on primary, you must re-create primary from 
-scratch (Using RMAN duplicate method). In this example, we have already enabled flashback on both primary and standby.
+Perform Failover to Standby
+===========================
+Failover is when your primary database is completely lost.  When there is a failover, 
+standby is converted into primary but primary is not converted into standby as it is lost. 
+If you do not have Flashback enabled on primary, you must re-create primary from 
+scratch (Using RMAN duplicate method). In this example, we have already enabled flashback 
+on both primary and standby.
 
   Crash Primary Database  
   Perform Failover to Standby
@@ -78,38 +81,43 @@ Physical Standby  = proddb_st
 
 Crash Primary database
 
-Let’s crash primary (proddb): In order to simulate failure, we will shut down the primary server proddb. As root user, shutdown 
-the server without shutting down DB.
+Let’s crash primary (proddb): In order to simulate failure, we will shut down 
+the primary server proddb. As root user, shutdown the server without shutting down DB.
 
-Execute query on client: At this stage, there is no primary to accept queries from client. Run below query on client putty terminal. 
-The query will hang and wait until standby is converted to primary
+Execute query on client: At this stage, there is no primary to accept queries from 
+client. Run below query on client putty terminal. The query will hang and wait 
+until standby is converted to primary
 
 select name, open_mode, db_unique_name, database_role from v$database;
 
 
 Perform Failover to Standby
 
-Minimize data loss (proddb): If you can mount the primary database, then flush the logs to standby
+Minimize data loss (proddb): If you can mount the primary database, then 
+flush the logs to standby
 
 On primary:
 ===========
 SQL> startup mount
 SQL> alter system flush redo to 'proddb_st';
 
-If you are not able to mount the database, then check if primary server is up. In that case manually copy archive logs from primary 
-to standby and register those logs on standby database
+If you are not able to mount the database, then check if primary server is up. In 
+that case manually copy archive logs from primary to standby and register those 
+logs on standby database
 
 On standby:
 ===========
 SQL> alter database register physical logfile '&logfile_path';
 
-Check for redo gaps: If any gap exists, copy log files from primary and register on standby as per last step
+Check for redo gaps: If any gap exists, copy log files from primary and register 
+on standby as per last step
 
 On standby:
 ===========
 SQL> select THREAD#, LOW_SEQUENCE#, HIGH_SEQUENCE# from V$ARCHIVE_GAP;
 
-Start failover: We need to activate standby so that client can continue to access even after failover
+Start failover: We need to activate standby so that client can continue to access 
+even after failover
 
 On standby:
 ===========
@@ -128,7 +136,7 @@ Check client query: Check the query you executed in step 2 on client, it must ge
 Rebuild Primary After Failover
 
 Post failover, there are two methods of rebuilding your failed primary
-  Method 1:Rebuild from scratch à RMAN duplicate
+  Method 1: Rebuild from scratch à RMAN duplicate
   Method 2: Flashback database à only if Flashback was enabled
 
 Note: In our earlier activity, we have performed Failover. Current state of your servers should be
