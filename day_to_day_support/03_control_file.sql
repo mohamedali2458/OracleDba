8923a4553fdaa523035074ff9b7cd88aa6c830e8
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2025-08-12 09:47:59 +0530

    Create 03_control_file.sql

---
+++ day_to_day_support/03_control_file.sql
@@ -0,0 +1,24 @@
+--backup of controlfile to trace
+SQL> alter database backup controlfile to trace as '/u01/backup.ctl';
+
+--Multiplexing the Control file
+
+set linesize 200
+col name for a90
+select name from v$controlfile;
+
+show parameter control_files;
+
+--we got 2 control files and we are adding the 3rd one
+sql> alter system set control_files='/u01/prod/control01.ctl', '/u01/prod/control02.ctl','/u01/prod/control03.ctl' SCOPE=spfile;
+
+SQL> shutdown immediate;
+
+$cp /u01/prod/control01.ctl /u01/prod/control03.ctl
+
+sql> startup
+
+set linesize 200
+col name for a90
+select name from v$controlfile;
+
