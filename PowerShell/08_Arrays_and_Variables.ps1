be4dc4b33487f5d3735bf951b41adc52fd8f760c
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2025-12-21 17:15:09 +0530

    Update 08_Arrays_and_Variables.ps1

--- PowerShell/08_Arrays_and_Variables.ps1
+++ PowerShell/08_Arrays_and_Variables.ps1
@@ -10,3 +10,10 @@ PowerShell is a powerful task automation and configuration management framework 
 - Working with files and folders
 - Managing Windows registry
 
+In PowerShell commands, known as cmdlets (pronounced "command-lets"), follow a specific structure:
+Verb-Noun
+- Verb: Represents the action you want to perform (e.g. Get, Set, Add, Remove)
+- Noun: Represents the object you want to work with (e.g. Process, Service, Item, ChildItem)
+
+Get-Service
+Get-Date
