8d7e99875f38a8a23b32846aed6881b37896f4ca
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2025-12-21 16:48:07 +0530

    myNotes.txt

---
+++ PowerShell/08_Arrays_and_Variables.ps1
@@ -0,0 +1,12 @@
+get-service
+get-date
+
+$psversiontable.psversion
+
+PowerShell is a powerful task automation and configuration management framework from Microsoft. Here's what you can do with it:
+- Managing user accounts and groups in Active Directory
+- Configuring network settings
+- Managing services and processes
+- Working with files and folders
+- Managing Windows registry
+
