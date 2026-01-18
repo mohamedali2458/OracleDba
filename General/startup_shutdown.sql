8d549abd02201c853f563c192e16c685e86f38b9
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2026-01-18 10:18:25 +0530

    Update startup_shutdown.sql

--- General/startup_shutdown.sql
+++ General/startup_shutdown.sql
@@ -24,3 +24,35 @@ shutdown;
 
 SYS @ oradb > startup pfile=initoradb.ora
 ORA-32006: SEC_CASE_SENSITIVE_LOGON initialization parameter has been deprecated
+
+
+
+
+
+
+
+Topic : Startup & Shutdown Modes
+================================
+Starting and stopping an Oracle database is a core DBA task. Understanding the phases 
+helps in maintenance, recovery, and troubleshooting.
+  
+💥 Startup Phases
+A) NOMOUNT
+ • Reads PFILE/SPFILE
+ • Allocates memory (SGA)
+ • Starts background processes
+B) MOUNT
+ • Opens control files
+ • Database knows its structure
+C) OPEN
+ • Opens datafiles & redo logs
+ • Database is ready for users
+
+💥 Shutdown Modes
+ A) NORMAL – Waits for users to disconnect
+ B) IMMEDIATE – Disconnects users, safe & common
+ C) TRANSACTIONAL – Waits for active transactions
+These are also known as Graceful shutdown.
+ D) ABORT – Force stop (used in emergencies)
+This is also known as Non-Graceful shutdown.
+  
