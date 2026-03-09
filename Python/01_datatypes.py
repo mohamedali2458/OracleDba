46e786df241155c0b79330a3e9625d72115d7ea8
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2026-03-09 16:19:39 +0530

    aa

---
+++ Python/01_datatypes.py
@@ -0,0 +1,19 @@
+Data Types
+
+Numeric
+
+Int, is a whole number, positive or negative,
+	without decimals, of unlimited length.
+
+Complex - numbers are of a real part and an imaginary part. 
+
+Numeric with decimal point:
+	Float is a number, positive or negative, containing
+	one or more decimals. 
+
+Alphanumeric:
+	String 	'Python', "123"
+
+Boolean:
+	Boolean	True and False
+
