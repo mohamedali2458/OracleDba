33355cc3f56f065d2465960b61d1cb49b7482621
mohamedali2458 <126348709+mohamedali2458@users.noreply.github.com>
2026-04-05 18:12:19 +0530

    Update ArchiveSpaceUtilization.sql

--- OncallSupport/ArchiveSpaceUtilization.sql
+++ OncallSupport/ArchiveSpaceUtilization.sql
@@ -1,22 +1,23 @@
 ArchiveSpaceUtilization
 
-select name, db_unique_name, open_mode, log_mode, database_role from v$database;
+SELECT name, db_unique_name, open_mode, log_mode, database_role FROM v$database;
 
 set linesize 300
 col destination for a50
-select destination from v$archive_dest where dest_name = 'LOG_ARCHIVE_DEST_1';
+SELECT destination FROM v$archive_dest 
+WHERE dest_name = 'LOG_ARCHIVE_DEST_1';
 
 archive log list;
 
 set linesize 300
 col value for a50
-select value from v$parameter where name = 'db_recovery_file_dest';
+SELECT value FROM v$parameter WHERE name = 'db_recovery_file_dest';
 
-if archive location is asm:
+--if archive location is ASM:
 
 set linesize 300
 col name for a30
-select group_number, name, state, type, round(total_mb/1024) total_gb,
+SELECT group_number, name, state, type, round(total_mb/1024) total_gb,
 round(free_mb/1024) free_gb, 100-round((100/total_mb)*free_mb,2) per_used
-from v$asm_diskgroup
-order by group_number;
+FROM v$asm_diskgroup
+ORDER BY group_number;
